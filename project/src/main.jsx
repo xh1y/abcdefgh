942de16d61c249440210296ae00f06b9edbc401c
Pansy <zhaowenjia@redrock.team>
2025-04-10 11:37:20 +0800

    finish page1

--- project/src/main.jsx
+++ project/src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
 import Page2 from './pages/page2/page2.jsx'
-
+import Page1 from './pages/page1/page1.jsx'
 import HostChart from './pages/page2/components/HostChart.jsx'
 import MedalChart from './pages/page2/components/MedalChart.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Page2 />
+    <Page1 />
   </StrictMode>,
 )
  
